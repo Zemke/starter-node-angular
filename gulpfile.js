613bd9b8d14c68d8c6dd69bfb523500897e6f489
Zemke <florian.zemke@ewetel.net>
2014-12-19 10:17:31 +0100

    Live reload for front-end files and server restart for back-end files.

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 
 var clean = require('gulp-clean');
 var concat = require('gulp-concat');
+var nodemon = require('gulp-nodemon');
+var livereload = require('gulp-livereload');
 
 var baseDirs = {
   app: './',
@@ -9,14 +11,16 @@ var baseDirs = {
 };
 
 var publicDirs = {
+  _self: 'public/',
   js: 'public/js/',
   css: 'public/css/',
   img: 'public/img/'
 };
 
-var files = {
+var appFiles = {
   js: [baseDirs.app + 'js/**/*.js'],
-  css: [baseDirs.app + 'css/**/*.css']
+  css: [baseDirs.app + 'css/**/*.css'],
+  index: [baseDirs.app + 'views/index.jade']
 };
 
 var concatFilenames = {
@@ -24,20 +28,53 @@ var concatFilenames = {
   css: 'css.css'
 };
 
+var startupScript = 'server.js';
+
 gulp.task('clean', function() {
   return gulp.src(baseDirs.dist).pipe(clean());
 });
 
 gulp.task('dev:concatjs', function () {
-  return gulp.src(files.js)
+  return gulp.src(appFiles.js)
     .pipe(concat(concatFilenames.js))
     .pipe(gulp.dest(baseDirs.app + publicDirs.js));
 });
 
 gulp.task('dev:concatcss', function () {
-  return gulp.src(files.css)
+  return gulp.src(appFiles.css)
     .pipe(concat(concatFilenames.css))
     .pipe(gulp.dest(baseDirs.app + publicDirs.css));
 });
 
-gulp.task('default', ['dev:concatjs', 'dev:concatcss']);
+gulp.task('nodemon', function () {
+  nodemon({
+      script: baseDirs.app + startupScript,
+      ext: 'js',
+      ignore: [
+        baseDirs.app + 'public/',
+        baseDirs.app + 'js/',
+        baseDirs.app + 'css/']
+    })
+    .on('restart', function () {
+      console.log('Magic restarted');
+    });
+});
+
+gulp.task('livereload', ['dev:concatjs', 'dev:concatcss'], function () {
+  return gulp.src(appFiles.index)
+    .pipe(livereload());
+});
+
+gulp.task('watch', function () {
+  livereload.listen();
+  gulp.watch([
+      appFiles.js,
+      appFiles.css,
+      baseDirs.app + '**/*.jade',
+    ], ['livereload'])
+    .on('change', function(event) {
+      console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+    });
+});
+
+gulp.task('default', ['dev:concatjs', 'dev:concatcss', 'nodemon', 'watch']);
